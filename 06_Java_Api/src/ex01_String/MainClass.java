7a70190b543a9ae15e2e0e671adc1337b84f8535
tjdalsl17 <1117ysm@naver.com>
2023-01-16 15:04:14 +0900

    2023-01-16

--- 06_Java_Api/src/ex01_String/MainClass.java
+++ 06_Java_Api/src/ex01_String/MainClass.java
@@ -2,7 +2,7 @@ package ex01_String;
 
 public class MainClass {
 	
-	public static void ex01() {
+	public static void literal() {
 		
 		// 문자열 리터럴(Literal)
 		String str1 = "hello";
@@ -12,34 +12,99 @@ public class MainClass {
 		// 동일한 리터럴("hello")를 2번 이상 사용하면
 		// 기존에 사용한 리터럴을 재사용한다.("hello"라는 리터럴이 2개 이상 만들어 지지 않는다.)
 		
+		/*
+        |-------|
+   str1 | 0x123 |
+        |-------|
+   str2 | 0x123 |
+        |-------|
+        |  ...  |
+        |-------|
+        |"hello"| 0x123
+        |-------|
+	*/
+		
+		
+		// 참조값이 같다는 의미이다. 저장된 문자열이 같다는 의미가 아니다.
+		System.out.println((str1 == str2));
+	}
+	
+	public static void stringObject() {
+		
+		// 문자열 객체
+		String str1 = new String("hello");
+		String str2 = new String("hello");
+		
+		// 문자열 객체는 언제나 새로 생성된다.
 		
 		/*
         |-------|
-   str1 | 0x123 |─────┐
-        |-------|          │
-   str2 | 0x123 |          │
-        |-------|          │
-   a[0] |   0   | 0x123    │
-        |-------|          │
-   a[1] |   0   |          │
-        |-------|          │
-   a[2] |   0   |          │ 
-        |-------|          │
-   a[3] |   0   |          │
-        |-------|          │
-   a[4] |   0   |          │
-        |-------|          │
-        |  ...  |          │
-        |-------|          │
-      b | 0x123 |◀────┘
+   str1 | 0x123 |
+        |-------|
+   str2 | 0x456 |
+        |-------|
+        |  ...  |
+        |-------|
+        |"hello"| 0x123
+        |-------|
+        |  ...	| 
+        |-------|
+        |"hello"| 0x456
         |-------|
 	*/
-		// 참조갑이 같다는 의미이다. 저장된 문자열이 같다는 의미가 아니다.
-		System.out.println((str1 == str2));
+		// 참조값이 다르다는 의미이다. 저장된 문자열이 다르다는 의미가 아니다.
+		System.out.println(str1 == str2);
+		
 	}
 
+	public static void equals() {
+		
+		// equals 메소드
+		// 비교하는 문자열이 동일하면 true, 아니면 false 반환
+		
+		String str1 = "hello";
+		String str2 = new String("hello");
+		
+		// 대소문자도 일치해야 함
+		if(str1.equals(str2)) {
+			System.out.println("str1과 str2는 같은 문자열이다.");
+		} else {
+			System.out.println("str1과 str2는 다른 문자열이다.");
+		}
+		
+		// 대소문자는 무시함
+		if(str1.equalsIgnoreCase(str2)) {
+			System.out.println("str1과 str2는 같은 문자열이다.");
+		} else {
+			System.out.println("str1과 str2는 다른 문자열이다.");
+		}
+	}
+	
+	public static void length() {
+		
+		// length 메소드
+		// 문자열의 길이(글자수)를 반환
+		
+		String str = "Hello\nWorld";
+		int length = str.length();	// arr.length : 필드, str.length() : 메소드
+		System.out.println("글자수 : " + length);
+		
+	}
+	
+	public static void charAt() {
+		
+		// charAt 메소드
+		// 문자열의 특정 인덱스의 문자(Char)를 반환
+		
+		String name = "윤성민";
+		for(int i = 0; i < name.length(); i++) {
+			System.out.println(name.charAt(i));
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		ex01();
+		charAt();
 
 	}
 
