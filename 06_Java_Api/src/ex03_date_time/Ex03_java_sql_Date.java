0f26cadc99b4d200b473009820149c6a95431f30
tjdalsl17 <1117ysm@naver.com>
2023-01-17 16:54:18 +0900

    2023-01-17

--- 06_Java_Api/src/ex03_date_time/Ex03_java_sql_Date.java
+++ 06_Java_Api/src/ex03_date_time/Ex03_java_sql_Date.java
@@ -6,6 +6,8 @@ public class Ex03_java_sql_Date {
 
 	public static void main(String[] args) {
 		
+		// Oracle 데이터베이스와 호환되는 java.sql.Date
+		
 		Date now = new Date(System.currentTimeMillis());
 		System.out.println(now);
 		
