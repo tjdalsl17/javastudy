d3acb7996deb33a7ff2c021760713c8daa3e31e8
tjdalsl17 <1117ysm@naver.com>
2023-01-25 17:40:02 +0900

    2023-01-25

--- 09_IO/src/practice/MainClass.java
+++ 09_IO/src/practice/MainClass.java
@@ -2,8 +2,12 @@ package practice;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainClass {
 	
@@ -81,8 +85,97 @@ public class MainClass {
 		System.out.println("파일 크기 : " + kb + "KB");  // 킬로 바이트 출력(1024바이트 = 1킬로바이트)
 	}
 	
+	public static void ex04() {
+		
+		File dir = new File("C:" + File.separator + "Program Files");
+		
+		File[] files = dir.listFiles();
+		
+		for(int i = 0; i < files.length; i++) {
+			if(files[i].isHidden() == false) { // 숨김 파일이 아니면 보여줌
+				System.out.println(files[i].getName());
+				
+			}
+		}
+	}
+	
+	// 문제2. C:\Program Files\Java\jdk-11.0.17 경로의 파일 목록을 아래와 같이 출력하시오.
+		/*
+		2023-01-04  오후 02:19    <DIR>          bin
+		2023-01-04  오후 02:19    <DIR>          conf
+		2023-01-04  오후 02:19    <DIR>          include
+		2023-01-04  오후 02:19    <DIR>          jmods
+		2023-01-04  오후 02:19    <DIR>          legal
+		2023-01-04  오후 02:19    <DIR>          lib
+		2023-01-04  오후 02:19               160 README.html
+		2023-01-04  오후 02:19             1,302 release
+		               2개 파일               1,462 바이트
+		*/
+	
+	public static void ex05() {
+		
+		File dir = new File("C:" + File.separator + "Program Files" + File.separator + "Java" + File.separator + "jdk-11.0.17");
+		
+		if(dir.exists()) {
+			
+			File[] list = dir.listFiles();
+			
+			int fileCount = 0;
+			long totalFileSize = 0;
+			for(File file : list) {
+				if(file.isHidden()) {
+					continue;
+				}
+				String lastModifiedDate = new SimpleDateFormat("yyyy-MM-dd  a hh:mm").format(file.lastModified());
+				String directory = file.isDirectory() ? "<Dir>" : "";
+				String size = "";
+				if(file.isFile()) {
+					long length = file.length();
+					size = new DecimalFormat("#,##0").format(file.length());
+					fileCount++;
+					totalFileSize += file.length();
+				}
+				String name = file.getName();
+				String result = String.format("%s%9s%9s %s\n", lastModifiedDate, directory, size, name);
+				System.out.print(result);
+				
+				
+				
+			}
+				System.out.println(fileCount + "개 파일 " + new DecimalFormat("#,##0").format(totalFileSize) + "바이트");
+				
+				
+		}
+			
+			
+	}
+		
+		
+		
+		
+		
+		
+	
+	
+	// 문제3. C:\storage 디렉터리를 삭제하시오.
+	// 파일이 저장된 디렉터리는 지워지지 않으므로 먼저 디렉터리에 저장된 파일을 삭제해야 한다.
+	
+	public static void ex06() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		
+		File file = new File(dir, "myfile.txt");
+		if(file.exists()) {
+			file.delete();
+		}
+		if(dir.exists()) {
+			dir.delete();
+		}
+	}
+	
+	
 	public static void main(String[] args) throws IOException { // main 메소드를 호출하는 곳으로 예외 처리를 넘긴다. (개발자가 try-catch 하지 않겠다.)
-		ex03();
+		ex06();
 
 	}
 
