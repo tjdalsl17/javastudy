c6c7fec2a7cd5faf1d78921113117686a0e40a25
tjdalsl17 <1117ysm@naver.com>
2023-01-30 12:38:20 +0900

    2023-01-30

--- 09_IO/src/ex04_OutputStream/MainClass.java
+++ 09_IO/src/ex04_OutputStream/MainClass.java
@@ -1,10 +1,14 @@
 package ex04_OutputStream;
 
 import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainClass {
 	
@@ -105,8 +109,92 @@ public class MainClass {
 		System.out.println("ex03.bin 파일의 크기 : " + file.length() + "바이트" );
 	}
 	
+	public static void ex04() {
+		
+		// 변수를 그대로 출력하는 DataOutputStream
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex04.dat");
+		
+		DataOutputStream dos = null;
+		
+		try {
+			
+			dos = new DataOutputStream(new FileOutputStream(file));
+			
+			// 출력할 변수
+			String name = "에밀리";  // 9바이트
+			int age = 30; 			 // 4바이트
+			double height = 170.5; 	 // 8바이트
+			boolean isAlive = true;  // 
+			
+			// 출력(변수 타입에 따라서 메소드가 다름)
+			dos.writeUTF(name);
+			dos.writeInt(age);
+			dos.writeDouble(height);
+			dos.writeBoolean(isAlive);
+			
+		} catch(IOException e) {
+			e.printStackTrace();	
+		} finally {
+			try {
+				if(dos != null) {
+					dos.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("ex04.dat 파일의 크기 : " + file.length() + "바이트");
+	}
+	
+	public static void ex05() {
+		
+		// 객체를 그대로 출력하는 ObjectOutputStream
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex05.dat");
+		
+		ObjectOutputStream oos = null;
+		
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(file));
+			
+			// 출력할 객체
+			List<Person> people = Arrays.asList(
+				new Person("에밀리", 30, 180.5, true),
+				new Person("제시카", 35, 190.5, true)
+			);
+			
+			// 출력할 객체
+			Person person = new Person();
+			person.setName("제임스");
+			person.setAge(40);
+			person.setHeight(170.5);
+			person.setAlive(false);
+			
+			// 출력
+			oos.writeObject(people);
+			oos.writeObject(person);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos != null) {
+					oos.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("ex05.dat 파일의 크기 : " + file.length() + "바이트" );
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		ex03();
+		ex05();
 
 	}
 
